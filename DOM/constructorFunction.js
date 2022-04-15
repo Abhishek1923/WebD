9d4270d6664f11877b37c0fa6f6d87c4c9400aa4
Abhishek1923 <abhishek230401@gmail.com>
2022-04-15 15:46:46 +0530

    Drum Kit

---
+++ DOM/constructorFunction.js
@@ -0,0 +1,10 @@
+function houseKeeper(name, yearsOfExp){
+	this.name=name;
+	this.yearsOfExp=yearsOfExp;
+}
+
+var houseKeeper1=new houseKeeper("Shakshi",51);
+
+console.log(houseKeeper1);
+
+console.log(houseKeeper1.name);
